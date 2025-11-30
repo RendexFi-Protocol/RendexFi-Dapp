91c03d05f8f6b9a70861c465fe5f4b1039fc7ae7
Whoisbezo.fx <bezzo19@gmx.de>
2025-11-30 01:10:47 +0100

    App.js aktualisieren

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Powerd by RendexFi Protocol
         </p>
         <a
           className="App-link"
