df927373043af93ec012b21ad167c8bcd625100f
Whoisbezo.fx <bezzo19@gmx.de>
2025-11-30 03:50:47 +0100

    App.js aktualisieren

--- src/App.js
+++ src/App.js
@@ -3,6 +3,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        {/* Topbar */}
+      <div className="topbar">
+        <div className="topbar-content">
+          {/* Links: Logo */}
+          <div className="topbar-left">
+            <div className="logo">DexTrade</div>
+          </div>
+
+          {/* Mitte: Suchleiste */}
+          <div className="topbar-center">
+            <div className="search-bar">
+              <input 
+                type="text" 
+                placeholder="Tokens, Pools suchen..."
+                className="search-input"
+              />
+              <button className="search-button">
+                🔍
+              </button>
+            </div>
+          </div>
+
+          {/* Rechts: Settings + Connect Wallet */}
+          <div className="topbar-right">
+            <button className="settings-button">
+              ⚙️
+            </button>
+            <button className="connect-wallet-button">
+              Connect Wallet
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
