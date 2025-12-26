38cc3964dda8af052878aed3f9e879248b7e6d69
Whoisbezo.fx <bezzo19@gmx.de>
2025-12-26 16:45:19 +0100

    Create TopButtons.jsx

---
+++ src/components/chrome/TopButtons.jsx
@@ -0,0 +1,16 @@
+import GlassButton from "../ui/GlassButton";
+import "./TopButtons.css";
+
+export default function TopButtons() {
+  return (
+    <div className="top-buttons">
+      <GlassButton className="top-button left">
+        
+      </GlassButton>
+
+      <GlassButton className="top-button right">
+        
+      </GlassButton>
+    </div>
+  );
+}
