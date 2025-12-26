83e88739ab3c34ebadb57f13f9641adacda6584b
Whoisbezo.fx <bezzo19@gmx.de>
2025-12-26 16:36:35 +0100

    Update GlassButton.jsx

--- components/ui/GlassButton.jsx
+++ components/ui/GlassButton.jsx
@@ -1,14 +1,11 @@
-export default function glassButton({
-  children,
-  className = "",
-  onClick,
-  ...props
-}) {
+import "./ui.css";
+
+export default function GlassButton({children, className = "", onClick, ...props }) {
   return (
-    <button 
-      className={`glass-button ${className}`}
-      onClick={onClick}
+    <button className={`glass-button ${className}`}onClick={onClick}
       {...props}
     >
       {children}
     </button>
+  );
+}
