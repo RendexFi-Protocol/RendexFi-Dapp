d9ff45897b21019321de9fb079077d2b031501c8
Whoisbezo.fx <bezzo19@gmx.de>
2025-12-26 17:49:13 +0100

    WalletCard.jsx aktualisieren

--- src/components/wallet/WalletCard.jsx
+++ src/components/wallet/WalletCard.jsx
@@ -2,11 +2,11 @@ import "./WalletCard.css";
 
 export default function WalletCard() {
   return (
-    <div className="wallet-card">
+    <div className="wallet--card">
       <div className="wallet-content">
         <span className="wallet-title">WALLET</span>
         <span className="wallet-balance">$3.400,23</span>
-      </div>
+     </div>
     </div>
   );
 }
